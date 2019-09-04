2df5aab89e616f447c57b6b68a2af0b7274f9156
Trinomads <trinomadsglobal@gmail.com>
2019-09-04 23:30:12 +0530

    updated code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: true,
-  api_url: 'https://wediina-api.herokuapp.com'
+  api_url: 'https://wediina-api.herokuapp.com',
+  venue_id:String,
+  vendor_id:String,
+  customer_id:String
 
 };
