9abe20c3535da268d22d6b478d89c4e2fad2a212
Trinomads <trinomadsglobal@gmail.com>
2019-09-17 13:56:37 +0530

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  // api_url: 'https://api-wediina.herokuapp.com',
-  api_url:"http://localhost:3000",
+  api_url: 'http://3.16.131.123:3000',
+  // api_url:"http://localhost:3000",
   venue_id:null,
   vendor_id:null,
   customer_id:null,
